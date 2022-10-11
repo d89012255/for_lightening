60b7a1ce27f904b8feb3bf29d2c8de7e6de0dd20
d89012255 <d89012255@gmail.com>
2022-10-11 11:56:37 +0800

    auto update

--- push.py
+++ push.py
@@ -1,13 +1,27 @@
+# from git import Repo
+# import time
+# from datetime import datetime
+# dirfile= "C:/xampp/htdocs/for_lightening/uploads"
+# repo=Repo(dirfile)
+# print(repo)
+# g=repo.git
+# g.add("--all")
+# now = datetime.now()
+# current_time = now.strftime("%H:%M:%S")
+# print(current_time)
+# g.commit('-m', current_time)
+# g.push()
+# print("Successful push!") 
+
+
 from git import Repo
-import time
-from datetime import datetime
-dirfile= "C:/xampp/htdocs/for_lightening/uploads"
-repo=Repo(dirfile)
-g=repo.git
+import os
+
+dirfile = os.path.abspath('C:/xampp/htdocs/for_lightening/uploads') # code的文件位置，我默认将其存放在根目录下
+repo = Repo(dirfile, search_parent_directories=True)
+
+g = repo.git
 g.add("--all")
-now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
-print(current_time)
-g.commit('-m', current_time)
+g.commit("-m auto update")
 g.push()
-print("Successful push!") 
+print("Successful push!")
