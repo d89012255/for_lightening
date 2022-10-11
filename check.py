9d477550cdbc4310a0bb4382c218caf73799e5e2
d89012255 <d89012255@gmail.com>
2022-10-11 15:28:03 +0800

    auto update

--- check.py
+++ check.py
@@ -29,7 +29,7 @@ class MyEventHandler(FileSystemEventHandler):
 
         g.add("--all",shell=True)
         g.commit("-m auto update",shell=True)
-        g.push("-f origin main",shell=True)
+        g.push("-u origin main",shell=True)
         print("Successful push!")
 
     def on_deleted(self, event):
